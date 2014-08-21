4c5b21f99a76d8219e2ba07da85889318d0851ac
Marc Slagle <marc.slagle@online-rewards.com>
2014-08-20 21:50:50 -0400

    a more advanced query, searching with joins

---
+++ recipe_three/README.md
@@ -0,0 +1,5 @@
+Recipe three:
+
+Create a new arehouse and move a few boxes there, then query users to see which ones have boxes at thew new location.
+
+Show the debug flag
---
+++ recipe_three/query.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/env perl
+
+use strict;
+# include our data objects
+use BlueBox::Data;
+
+# All users who have a box a the secondary warehouse
+my $users = BlueBox::Data::Account::Manager->get_objects(
+                    require_objects => [ 'box' ],
+                    query => [ 'box.warehouse_id' => 2 ],
+                    debug => 1,
+            );
+foreach my $user (@$users)
+{
+    print "User " . $user->id . " " . $user->username . " has boxes at warehouse 2\n";
+}
+
+
